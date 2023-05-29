a0db6afadb6ad2bf3de3f70dee8f0f53642adf06
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-05-29 19:15:33 +0200

    client: add interface add simple interface App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import emptyMap from './empty-map.jpg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={emptyMap} className="img" alt="empty map" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Add new binary file please
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>
+          Add file
+        </button>
       </header>
     </div>
   );
