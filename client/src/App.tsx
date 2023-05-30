342ac1f6c6741c5b646fa00eb44b77b7ba6b11fd
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-05-30 14:38:59 +0200

    client: app.ts : add check for file type In function handleFileChange add check for upload file type

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
-import React, {useRef}from 'react';
+import React, {useState, useRef}from 'react';
 import emptyMap from './empty-map.jpg';
 
 import './App.css';
 
 function App() {
+  const [errors, setErrors] = useState<string | null>();
   const handleFileChange = async (event: any) => {
-    console.log(event.target.files);
+      const fileName:string=event.target.files[0].name
+      const fileType:string=fileName.split('.').reverse()[0]
+      if(fileType==='grid'){
+          setErrors(null)
+      }else{
+        setErrors("Please add correct file with type \'grid\'")
+      }
+  }
+  const handleClick = () => {
+    setErrors(null)
+    inputFileRef.current.click()
   }
   const inputFileRef:any = useRef(null);
   return (
@@ -15,8 +26,9 @@ function App() {
         <p>
           Add new binary file please
         </p>
+          {errors?<p>{errors}</p>:""}
         <button
-          onClick={()=>{inputFileRef.current.click()}}
+          onClick={handleClick}
         >
           Add file
         </button>
